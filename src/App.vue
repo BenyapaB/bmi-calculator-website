f28e987da33ac6a578a2f2738ecb8e63040b2da8
BenyapaB <fon.benyapa@gmail.com>
2024-09-29 12:59:34 +0700

    Create BMI Calculator website

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import BmiView from "./views/BmiView.vue";
+</script>
+
+<template>
+  <BmiView />
+</template>
+
+<style scoped>
+@font-face {
+  font-family: "Petchlamoon";
+  src: url(./fonts/Petchlamoon-Regular.ttf);
+}
+</style>
